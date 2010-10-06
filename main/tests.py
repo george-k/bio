59009a1ff604d5f66e09b3410594d3710e918029
George Kolesnikov <george.kolesnikov@gmail.com>
2010-10-06 21:29:42 +0300

    ticket 5: add test for contact edit form

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import date
 from tddspry.django import DatabaseTestCase, HttpTestCase
 from django.core.urlresolvers import reverse
 from main.models import Contact
@@ -47,3 +48,31 @@ class TestRequestContext(HttpTestCase):
         response = self.client.get('/')
         self.assert_true('settings' in response.context[0],
                          'No settings in context.')
+
+
+class TestContactEdit(HttpTestCase):
+    """ Test contact edit form work """
+
+    def contact_edit_test(self):
+        #Prepare test data
+        TEST_DATA = {'name': 'test_name',
+                     'surname': 'test_surname',
+                     'birthday': '01.01.2010',
+                     'bio': 'test_bio',
+                     'email': 'test@test.com',
+                     'phone': '1234567890'
+                     }
+        #Send post request to edit contact
+        response = self.client.post('/edit_contact/', TEST_DATA)
+        #Get edited contact
+        contact = Contact.objects.get(pk=1)
+        #Check contact members with test dict fields
+        self.assert_equal(contact.name, TEST_DATA['name'])
+        self.assert_equal(contact.surname, TEST_DATA['surname'])
+        self.assert_equal(contact.birthday.strftime("%d.%m.%Y"),
+                          TEST_DATA['birthday'])
+        self.assert_equal(contact.bio, TEST_DATA['bio'])
+        self.assert_equal(contact.email, TEST_DATA['email'])
+        self.assert_equal(contact.phone, TEST_DATA['phone'])
+        
+        
