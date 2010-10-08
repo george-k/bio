33d6e61e467a584696e79f080d5acfe108b88422
George Kolesnikov <george.kolesnikov@gmail.com>
2010-10-08 15:17:56 +0300

    ticket 12: add last 10 request logs page

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import url, patterns
+from django.views.generic import list_detail
 
+from reqlog.models import RequestLog
+
+
+last_10_requests = {'queryset': RequestLog.objects.order_by('-date')[0:10],
+                    'template_name': 'last_10_requests.html'}
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'show_contact'),
     url(r'^edit_contact/$', 'edit_contact'),
+    url(r'^request_logs_list/$', list_detail.object_list, last_10_requests,
+        name="last_logs"),
 )
 
 urlpatterns += patterns('django.contrib.auth.views',
