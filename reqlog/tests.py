5c662e39f457780f5b81e5d351dc36d66a7d53fc
George Kolesnikov <george.kolesnikov@gmail.com>
2010-10-11 16:35:35 +0300

    ticket 13: add test for request log priority

--- reqlog/tests.py
+++ reqlog/tests.py
@@ -22,6 +22,21 @@ class TestRequestLogging(HttpTestCase):
         self.assert_not_equal(last_log.req_str.find('logging'), -1)
         self.assert_not_equal(last_log.path.find('test_path'), -1)
 
+    def test_priority_field(self):
+        """ Check log priority field """
+        #Send request
+        self.client.get('/test_path/?teststring')
+        #Check last log priority
+        #(must be priority member and it must be equal to default value)
+        last_logs = RequestLog.objects.order_by('-date')[0:1]
+        self.assert_true(len(last_logs), 'Log not found')
+        last_log = last_logs[0]
+        self.assert_not_equal(last_log.req_str.find('teststring'), -1)
+        try:
+            self.assert_equal(last_log.priority, 1)
+        except:
+            self.assert_true(False, 'Request log priority failed')
+
 
 class TestObjectActionLogging(HttpTestCase):
     """ Check object action log record creation """
